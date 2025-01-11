9c6bbff35a21fe3575d5c33d6c26690be8b935dc
Prince Gupta <princegupta1586@gmail.com>
2025-01-12 00:51:16 +0530

    Apply Rotary Embeddings

--- llama2/utils.py
+++ llama2/utils.py
@@ -27,3 +27,21 @@ def precompute_theta_pos_frequencies(
     freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
     
     return freqs_complex
+
+def apply_rotary_embedings(x: torch.Tensor, freqs_complex: torch.Tensor, device: str):
+    # (B, seq_len, H, head_dim) -> (B, seq_len, H, head_dim / 2)
+    x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+    
+    # (seq_len, head_dim / 2) -> (1, seq_len, 1, head_dim / 2)
+    freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
+
+    # (B, seq_len, H, head_dim / 2) * (1, seq_len, 1, head_dim / 2) = (B, seq_len, H, head_dim / 2)
+    x_rotated = x_complex * freqs_complex
+
+    # (B, seq_len, H, head / 2) -> (B, seq_len, H, head_dim / 2, 2)
+    x_out = torch.view_as_real(x_rotated)
+
+    # (B, seq_len, H, head_dim / 2, 2) -> (B, seq_len, H, head_dim)
+    x_out = x_out.reshape(*x.shape)
+
+    return x_out.type_as(x).to(device)
