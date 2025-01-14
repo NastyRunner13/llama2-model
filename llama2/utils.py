da4e9561c08ebdb12b0d66134c2bc8939b8cf3aa
Prince Gupta <princegupta1586@gmail.com>
2025-01-14 22:18:38 +0530

    Add Repeat KV function

--- llama2/utils.py
+++ llama2/utils.py
@@ -45,3 +45,15 @@ def apply_rotary_embedings(x: torch.Tensor, freqs_complex: torch.Tensor, device:
     x_out = x_out.reshape(*x.shape)
 
     return x_out.type_as(x).to(device)
+
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    batch_size, seq_len, n_kv_heads, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    else:
+        return (
+            # (B, seq_len, n_kv_heads, 1, head_dim)
+            x[:, :, :, None, :]
+            .expand(batch_size, seq_len, n_kv_heads, n_rep, head_dim)
+            .reshape(batch_size, seq_len, n_kv_heads * n_rep, head_dim)
+        )
