16201c4adbe13099cc6c434644190e0574c9f221
Prince Gupta <princegupta1586@gmail.com>
2025-01-15 22:39:40 +0530

    Add transformer skeleton

---
+++ llama2/transformer.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+from rmsnorm import RMSNorm
+from model_args import ModelArgs
+from encoder_block import EncoderBlock
+from utils import precompute_theta_pos_frequencies
+
+class Transformer(nn.Module):
+
+    def __init__(self, args: ModelArgs) -> None:
+        super().__init__()
+
+        assert args.vocab_size != -1, "Vocab size must be set"
+
+        self.args = args
+        self.vocab_size = args.vocab_size
+        self.n_layers = args.n_layers
+        self.tok_embeddings = nn.Embedding(self.vocab_size, args.dim)
+
+        self.layers = nn.ModuleList()
+        for _ in range(args.n_layers):
+            self.layers.append(EncoderBlock(args))
+
+        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.output = nn.Linear(args.dim, self.vocab_size, bias=False)
+
+        self.freqs_complex = precompute_theta_pos_frequencies(
+            self.args.dim // self.args.n_heads, self.args.max_seq_len * 2, device = self.args.device
+        )
+
+    def forward(self, tokens: torch.Tensor, start_pos: int):
+        # (B, seq_len)
+        batch_size, seq_len = tokens.shape
+        assert seq_len == 1, "Only one token at a time can be processed"
+
+        # (B, seq_len) -> (B, seq_len, dim)
+        h = self.tok_embeddings(tokens)
+
+        # Retrieve the pairs (m, theta) corresponding to the positions [start_pos, start_pos + seq_len]
+        freqs_complex = self.freqs_complex[start_pos : start_pos + seq_len]
+
+        # Connexutively apply all the encoder layers
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_complex)
+        h = self.norm(h)
+        output = self.output(h).float()
+        
+        return output
