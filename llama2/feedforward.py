a036c8a18a3d3b6ddd78845d865cd25bdab65842
Prince Gupta <princegupta1586@gmail.com>
2025-01-17 18:16:58 +0530

    Complete Feed Forward Block

--- llama2/feedforward.py
+++ llama2/feedforward.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from model_args import ModelArgs
 
 class FeedForward(nn.Module):
@@ -14,4 +15,18 @@ class FeedForward(nn.Module):
             hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
 
         # Round the hidden_dim to the nearest multiple of the mutiple_of parameter
+        hidden = args.multiple_of * ((hidden_dim + args.multiple_of - 1) // args.multiple_of)
         
+        # hidden_size = 7, multiple_of = 5
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+
+    def forward(self, x: torch.Tensor):
+
+        swish = F.silu(self.w1(x))
+        x_V = self.w3(x)
+        x = swish * x_V
+        x = self.w2(x)
+
+        return x
